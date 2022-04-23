ddda8e97f116974981be58effb87722984792a96
Julio Zaldaña <julizaldana@gmail.com>
2022-04-23 08:01:20 -0600

    Actualización en clases _Model de libro y prestamista

--- libro/models/libro_Model.py
+++ libro/models/libro_Model.py
@@ -10,3 +10,61 @@ class Libro():
         self.nocopias = nocopias
         self.estanteria = estanteria
         self.fila = fila
+
+    def agregarCopias(self, cantidadcopias):
+        if(cantidadcopias > 0):
+            self.nocopias += cantidadcopias
+        else:
+            return{
+
+            }
+    
+    def restarCopias(self):
+        self.nocopias = self.nocopias - 1
+
+    def devolverCopia(self):
+        self.nocopias = self.nocopias + 1
+
+    def getIsbn(self):
+        return self.isbn
+    
+    def getAutor(self):
+        return self.autor
+
+    def getAño(self):
+        return self.año
+
+    def getTitulo(self):
+        return self.titulo
+
+    def getEdicion(self):
+        return self.edicion
+    
+    def getTemas(self):
+        return self.temas
+    
+    def getNoCopias(self):
+        return self.nocopias
+
+    def getEstanteria(self):
+        return self.estanteria
+
+    def getFila(self):
+        return self.fila
+
+
+    def getData(self):
+        return {
+            "isbn": self.isbn,
+            "autor": self.autor,
+            "año": self.año,
+            "titulo": self.titulo,
+            "edicion": self.edicion,
+            "temas": self.temas,
+            "copias": self.nocopias,
+            "estanteria": self.estanteria,
+            "fila": self.fila
+
+        }
+
+        
--- prestamista/models/prestamista_Model.py
+++ prestamista/models/prestamista_Model.py
@@ -3,3 +3,29 @@ class Prestamista():
         self.CUI = CUI
         self.nombre = nombre
         self.apellido = apellido
+    
+    def prestarLibros(self):
+        global libroprestado
+        libroprestado = True
+
+    def devolverLibros(self):
+        global libroprestado
+        libroprestado = False
+
+    def getCUI(self):
+        return self.CUI
+    
+    def getNombre(self):
+        return self.nombre
+
+    def getApellido(self):
+        return self.apellido
+
+    
+
+    def getData(self):
+        return{
+            "cui": self.CUI,
+            "nombre": self.nombre,
+            "apellido": self.apellido
+        }
